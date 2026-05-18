20f7344a8c80944222ba416fca91d523999308d6
gabireisch <114270405+gabireisch@users.noreply.github.com>
2026-05-18 15:23:53 -0300

    Semana 3

---
+++ infinite_dataset_test.py
@@ -0,0 +1,39 @@
+import torch
+
+from qusi.data import LightCurveDataset
+from qusi.data import LightCurveObservationCollection
+from qusi.model import Hadryss
+from qusi.session import get_device, infinite_datasets_test_session
+from torch.nn import BCELoss
+from torchmetrics.classification import BinaryAccuracy, BinaryAUROC
+
+from dataset import get_positive_test_paths, load_times_and_fluxes_from_path, positive_label_function, \
+    get_negative_test_paths, negative_label_function
+
+
+def main():
+    positive_test_light_curve_collection = LightCurveObservationCollection.new(
+        get_paths_function=get_positive_test_paths,
+        load_times_and_fluxes_from_path_function=load_times_and_fluxes_from_path,
+        load_label_from_path_function=positive_label_function)
+    negative_test_light_curve_collection = LightCurveObservationCollection.new(
+        get_paths_function=get_negative_test_paths,
+        load_times_and_fluxes_from_path_function=load_times_and_fluxes_from_path,
+        load_label_from_path_function=negative_label_function)
+
+    test_light_curve_dataset = LightCurveDataset.new(
+        standard_light_curve_collections=[positive_test_light_curve_collection,
+                                          negative_test_light_curve_collection])
+
+    model = Hadryss.new()
+    device = get_device()
+    model.load_state_dict(torch.load('sessions/<wandb_run_name>_latest_model.pt', map_location=device))
+    metric_functions = [BinaryAccuracy(), BCELoss(), BinaryAUROC()]
+    results = infinite_datasets_test_session(test_datasets=[test_light_curve_dataset], model=model,
+                                             metric_functions=metric_functions, batch_size=100, device=device,
+                                             steps=100)
+    return results
+
+
+if __name__ == '__main__':
+    main()
--- train.py
+++ train.py
@@ -1,21 +1,17 @@
 from qusi.model import Hadryss
 from qusi.session import TrainHyperparameterConfiguration, train_session
+
 from dataset import get_transit_train_dataset, get_transit_validation_dataset
 
-
 def main():
-    train_dataset = get_transit_train_dataset()
-    validation_dataset = get_transit_validation_dataset()
-    model = Hadryss.new(input_length=4000)
-
-    hyperparameters = TrainHyperparameterConfiguration.new(batch_size=4,cycles=2,train_steps_per_cycle=5,validation_steps_per_cycle=2)
-
-    train_session(
-        train_datasets=[train_dataset],
-        validation_datasets=[validation_dataset],
-        model=model,
-        hyperparameter_configuration=hyperparameters)
+    train_light_curve_dataset = get_transit_train_dataset()
+    validation_light_curve_dataset = get_transit_validation_dataset()
+    model = Hadryss.new()
+    train_hyperparameter_configuration = TrainHyperparameterConfiguration.new(
+        batch_size=100, cycles=20, train_steps_per_cycle=100, validation_steps_per_cycle=10)
+    train_session(train_datasets=[train_light_curve_dataset], validation_datasets=[validation_light_curve_dataset],
+                  model=model, hyperparameter_configuration=train_hyperparameter_configuration)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
